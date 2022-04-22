2a275346fa608ebf3ccd0c6e527fb2704065cf66
“coadaro” <coadaro@gmail.com>
2022-04-22 10:45:10 -0400

    AJAXandAPIsExerciseWithBonus

--- app.js
+++ app.js
@@ -40,14 +40,63 @@ const third = document.querySelector('#third');
 const p5 = document.createElement('p');
 const p6 = document.createElement('p');
 third.append(p5, p6);
-//3a
-const baseURL1 = `https://friends-quotes-api.herokuapp.com`;
-const endpoint1 = `/quotes/`;
-const fullEndpoint1 = baseURL1 + endpoint1;
+
 //3b
-async function quoteFunc
+async function quoteFunc(){
+    try{
+        //3a
+        const quoteJS3 = await axios.get(`https://friends-quotes-api.herokuapp.com/quotes/random`);
+        //3c
+        p5.innerText = quoteJS3.data.character;
+        p6.innerText = quoteJS3.data.quote;
+        //3d
+    } catch (err) {
+        console.log(err);
+    }
+    
+}
+quoteFunc()
 
 // 4
 const fourth = document.querySelector('#fourth');
 const p7 = document.createElement('p');
 fourth.append(p7);
+
+//4a
+async function tvMazeFunc(){
+    try{
+        const episodeByNum = await axios.get(`https://api.tvmaze.com/shows/38963/episodebynumber?season=2&number=8`)
+        p7.innerText = episodeByNum.data.name;
+        //4b
+    } catch (err){
+        console.log(err);
+    }
+}
+tvMazeFunc();
+
+//BONUS
+//5
+const body = document.querySelector(`body`);
+axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+.then(res => {
+    const img = document.createElement(`img`);
+    img.src = res.data.sprites.front_shiny;
+    body.append(img);
+})
+.catch (err => {
+    console.log(err);
+    alert(`Pokemon escaped!`)
+})
+
+//6
+async function bonusSix(){
+    try{
+        const moviePoster = await axios.get(`http://www.omdbapi.com/?apikey=b8b37900&t=fantastic+beasts`);
+        const poster = document.createElement(`img`);
+        poster.src = moviePoster.data.Poster;
+        body.append(poster);
+    } catch (err) {
+        console.log(err);
+    }
+}
+bonusSix()
